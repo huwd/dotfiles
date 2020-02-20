37a0bc9457ac357ed8ce8594f693113094baa8de
Huw Diprose <mail@huwdiprose.co.uk>
2020-02-20 21:15:29 +0000

    Add zsh alias config method to detect platform

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,13 @@
 
 # Don't change. The following determines where dotfiles is installed.
 dotfiles=$HOME/.dotfiles
+
+# Get operating system
+platform='unknown'
+unamestr=$(uname)
+if [[ $unamestr == 'Linux' ]]; then
+  platform='linux'
+elif [[ $unamestr == 'Darwin' ]]; then
+  platform='darwin'
+fi
+
