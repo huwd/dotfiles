b8686956553bc400182ceb67d3657bdebe5fa43f
Huw Diprose <mail@huwdiprose.co.uk>
2020-02-20 21:18:12 +0000

    Update zsh alias, friendly colourful ls, ll and list grep combo
    Colourful listing is prefereable, this setup will work with both mac and linux. The list and grep combo will save a lot of time. Will want to work out down the line if I want to use grep or something like ag or ripgrep

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,3 +16,14 @@ fi
 alias df='df -h'
 alias du='du -h -d 2'
 
+if [[ $platform == 'linux' ]]; then
+  alias ll='ls -alh --color=auto'
+  alias ls='ls --color=auto'
+elif [[ $platform == 'darwin' ]]; then
+  alias ll='ls -alGh'
+  alias ls='ls -Gh'
+fi
+
+# show me files matching "ls grep"
+alias lsg='ll | grep'
+
