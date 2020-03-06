73abafbaede63195e5cb8bf4d8719276ea1ffc9f
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-03-06 17:06:02 +0000

    Fix ASDF zsh config to work between mac and linux

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -1,1 +1,14 @@
-source $(brew --prefix asdf)/asdf.sh
+# Get operating system
+platform='unknown'
+unamestr=$(uname)
+if [[ $unamestr == 'Linux' ]]; then
+  platform='linux'
+elif [[ $unamestr == 'Darwin' ]]; then
+  platform='darwin'
+fi
+
+if [[ $platform == 'linux' ]]; then
+  source $HOME/.asdf/asdf.sh
+elif [[ $platform == 'darwin' ]]; then
+  source $(brew --prefix asdf)/asdf.sh
+fi
