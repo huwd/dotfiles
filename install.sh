0962624a00baa51504ff70787bb387eec20e21ba
huw <huw@grey-area.local>
2020-03-11 12:17:16 +0000

    Add install script dependency checks

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
+command -v rake >/dev/null 2>&1 || { echo >&2 "I require rake but it's not installed.  Aborting."; exit 1; }
 
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing Dotfiles for the first time"
