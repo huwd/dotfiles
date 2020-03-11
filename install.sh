322a340d7a3e32b090159a96c71eac2a4cae7780
huw <huw@grey-area.local>
2020-03-11 14:00:18 +0000

    Require zsh to run the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
 command -v rake >/dev/null 2>&1 || { echo >&2 "I require rake but it's not installed.  Aborting."; exit 1; }
+command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh but it's not installed.  Aborting."; exit 1; }
+
+if [ "$SHELL" != '/bin/zsh']; then
+    echo "This script will require zsh to run"
+    echo "Change to zsh shell by running:"
+    echo "zsh"
+    echo ""
+    echo "After which re-run this script"
+fi
 
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing Dotfiles for the first time"
