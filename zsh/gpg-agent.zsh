dc07046ee6d878a7696e8a95308e2d74d01ab96d
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-08-19 13:47:41 +0100

    Properly source gpg agent

--- zsh/gpg-agent.zsh
+++ zsh/gpg-agent.zsh
@@ -1,5 +1,8 @@
 # Set SSH to use gpg-agent
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-export SSH_AUTH_SOCK="~/.gnupg/S.gpg-agent.ssh"
+ export SSH_AUTH_SOCK="~/.gnupg/S.gpg-agent.ssh"
 fi
+
+eval $(ssh-agent) &>/dev/null
+ssh-add ~/.ssh/* &>/dev/null
