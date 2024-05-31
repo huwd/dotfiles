f6eded316279e6e24e2b31bb1fe00b781a165c2c
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2024-05-31 08:14:42 +0100

    Remove unused aliases and tools

--- zsh/linuxbrew.zsh
+++
@@ -1,2 +0,0 @@
-# Add Linuxbrew to shellenv
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
--- zsh/ruby.zsh
+++
@@ -1,4 +0,0 @@
-export PATH="$HOME/.dotfiles/rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-export PATH="$HOME/.dotfiles/rbenv/plugins/ruby-build/bin:$PATH"
-export RBENV_ROOT="$HOME/.dotfiles/rbenv"
